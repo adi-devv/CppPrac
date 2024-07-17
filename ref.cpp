51161b49b56c9654423f402cee98a332c99a367a
adi-devv <aaditsingal7859@gmail.com>
2024-07-17 17:53:58 +0530

    a

---
+++ ref.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int& increment(int &x) {
+    x++;
+    return x;
+}
+
+int main() {
+    int a = 10;
+    int &aref = a;                         // independent ref
+    int &ref = increment(aref);           // ref func
+    
+    cout << "a: " << aref << endl;  
+    cout << "ref: " << ref << endl; 
+    
+    ref++;
+    cout << "a: " << aref << endl;
+    cout << "ref: " << ref << endl;
+
+    return 0;
+}
