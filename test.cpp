444069c9aa937e80d52afa61e7b1e0efa0309e6b
adi-devv <aaditsingal7859@gmail.com>
2024-07-17 11:41:20 +0530

    init

---
+++ test.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+template<typename... Args>
+void print(const Args&... args) {
+    ((std::cout << args << ' '), ...);
+    std::cout << std::endl;
+}
+
+class Human {
+    float dna;
+
+public:
+    string name;
+    Human(const string& n) : name(n) {
+        print("\nCreating Human: ", name);
+    }
+
+    void walk(float speed) {
+        print("Walking.. at ", speed, "km/h");
+    }
+    void walk(int distance) {
+        print("Walking.. for ", distance, "km");
+    }
+    void owalk();
+
+    ~Human() {
+        print("\n------I'm Removed!");
+    }
+};
+
+void Human::owalk() {
+    print("Outside walking..");
+}
+
+class SubHuman : public Human {
+public:
+    string weapon;
+    SubHuman(const string& n) : Human(n) {
+        weapon = "Cape";
+    }
+    void fly() {
+        print("Now I'm flying");
+    }
+};
+
+int main() {
+    print("HI HUIHUIHUI");
+
+    string a = "aaa";
+    print("hhaaha", a);
+
+    Human hu("h");
+    hu.walk(4.5f);
+    hu.walk(20);
+    hu.owalk();
+
+    Human* hu2 = new Human("a");
+    hu2->walk(4);
+
+    SubHuman subhu("hahahsubhu");
+    subhu.walk(700.0f);
+    print(subhu.weapon);
+
+    delete hu2;
+    return 0;
+}
